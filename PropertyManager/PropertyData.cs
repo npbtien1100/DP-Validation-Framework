c361eec2c25224126a9d121efb7a64497ff33da4
FoveIT <tranvantu7597@gmail.com>
2022-01-17 21:31:10 +0700

    Add Property Manager

---
+++ PropertyManager/PropertyData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System;
+using System.Linq;
+
+namespace ValidationFramework
+{
+    /// <summary>
+    /// Property data.
+    /// </summary>
+    public class PropertyData
+    {
+        public PropertyData(PropertyInfo propertyInfo, IEnumerable<ValidationAttribute> validationAttributes)
+            : this()
+        {
+            propertyInfo.CannotBeNull();
+            validationAttributes.CannotBeNull();
+
+            this.PropertyInfo = propertyInfo;
+            this.ValidationAttributes = validationAttributes;
+        }
+
+        private PropertyData()
+        {
+        }
+
+        #region Public Properties
+        public PropertyInfo PropertyInfo
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<ValidationAttribute> ValidationAttributes
+        {
+            get;
+            private set;
+        }
+
+        #endregion Public Properties
+    }
+}
---
+++ PropertyManager/PropertyManager.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using System.Linq;
+
+namespace ValidationFramework
+{
+    /// <summary>
+    /// Reflection extension methods.
+    /// </summary>
+    public class PropertyManager
+    {
+        private static PropertyManager instance;
+        private Dictionary<Type, Dictionary<string, PropertyData>> typeProperties = new Dictionary<Type, Dictionary<string, PropertyData>>();
+
+        private PropertyManager()
+        {
+        }
+        public static PropertyManager getInstance()
+        {
+            if (instance == null)
+                instance = new PropertyManager();
+            return instance;
+        }
+        
+        public Dictionary<string, PropertyData> GetProperties(Type type)
+        {
+            // TODO: should include fields?
+            BindingFlags bindings = BindingFlags.Public | BindingFlags.Instance;
+
+            if (!typeProperties.TryGetValue(type, out Dictionary<string, PropertyData> properties))
+            {
+                properties = new Dictionary<string, PropertyData>();
+
+                // get property information
+                foreach (var propertyInfo in type.GetProperties(bindings))
+                {
+                    properties.Add(propertyInfo.Name, new PropertyData(propertyInfo, propertyInfo.GetCustomAttributes<ValidationAttribute>(true).Cast<ValidationAttribute>().ToList()));
+                }
+
+                // cache property information
+                typeProperties.Add(type, properties);
+            }
+
+            return properties;
+        }
+    }
+}
